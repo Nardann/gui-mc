97b112f0f9c64af8ae37123f77b6a071c7d0ab01
Nardann <marclorente29@gmail.com>
2023-11-26 17:35:42 +0100

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,20 +1,29 @@
 // public/app.js
 
 $(document).ready(function () {
-    // ...
+    let activeConfigMenu = null;
   
     // Gestionnaire de clic sur les éléments de l'inventaire
     $('.inventory').on('click', '.inventory-item', function (e) {
-      // Si le clic est un clic droit, ignorer
-      if (e.which === 3) return;
-  
-      showConfigMenu($(this));
+        // Si le clic est un clic droit, ignorer
+        if (e.which === 3) return;
+
+        if (activeConfigMenu) {
+        hideConfigMenu(); // Cacher le menu actif s'il y en a un
+        }
+
+        showConfigMenu($(this));
     });
   
     // Gestionnaire de clic droit pour ouvrir le menu de configuration
     $('.inventory').on('contextmenu', '.inventory-item', function (e) {
-      e.preventDefault();
-      showConfigMenu($(this));
+        e.preventDefault();
+
+        if (activeConfigMenu) {
+        hideConfigMenu(); // Cacher le menu actif s'il y en a un
+        }
+
+        showConfigMenu($(this));
     });
   
     // Gestionnaire de clic sur le document pour masquer le menu de configuration
@@ -59,14 +68,15 @@ $(document).ready(function () {
 
     // Fonction pour afficher le menu de configuration
     function showConfigMenu(item) {
-      const configMenu = $('<div class="config-menu">Config menu for ' + item.text() + '</div>');
-      item.append(configMenu);
-      configMenu.show();
+        activeConfigMenu = $('<div class="config-menu">Config menu for ' + item.text() + '</div>');
+        item.append(activeConfigMenu);
+        activeConfigMenu.show();
     }
   
     // Fonction pour masquer le menu de configuration
     function hideConfigMenu() {
-      $('.config-menu').hide().remove();
+        activeConfigMenu.hide().remove();
+        activeConfigMenu = null; // Réinitialiser la variable du menu actif
     }
 
     for (let i = 0; i < 3; i++) {
