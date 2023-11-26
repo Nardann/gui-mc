3f268199d4ecd8e03f2c4faf584904cf74d56b7e
Nardann <94087736+Nardann@users.noreply.github.com>
2023-11-26 18:33:46 +0100

    Update and rename app.js to inventory.js

---
+++ public/inventory.js
@@ -0,0 +1,43 @@
+// public/app.js
+
+$(document).ready(function () {
+    // Gestionnaire de clic sur les boutons Ajouter/Supprimer (utilisation de délégués d'événements)
+    $('#addRowBtn').on('click', function () {
+      if ($('.inventory-row').length < 6) {
+        addInventoryRow();
+      }
+    });
+  
+    $('#removeRowBtn').on('click', function () {
+      removeInventoryRow();
+    });
+  
+    // Fonction pour ajouter une ligne d'inventaire
+    function addInventoryRow() {
+      const inventory = $('.inventory');
+      const newRow = $('<div class="inventory-row"></div>');
+  
+      for (let i = 0; i < 9; i++) {
+        const itemIndex = inventory.children('.inventory-row').length * 9 + i;
+        const item = $(`<div class="inventory-item">Item ${itemIndex}</div>`);
+        newRow.append(item);
+      }
+  
+      inventory.append(newRow);
+    }
+  
+    // Fonction pour supprimer une ligne d'inventaire
+    function removeInventoryRow() {
+      const inventory = $('.inventory');
+      const rows = inventory.children('.inventory-row');
+  
+      if (rows.length > 1) {
+        rows.last().remove();
+      }
+    }
+    // Ajouter trois lignes d'inventaire au démarrage
+    for (let i = 0; i < 3; i++) {
+      addInventoryRow();
+    }
+  });
+  
