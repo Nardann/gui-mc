68be30903055552feb4493dd0b12adfa0eecb81a
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-20 23:27:53 +0100

    Update inventory.js

--- public/inventory.js
+++ public/inventory.js
@@ -1,43 +1,45 @@
-// public/app.js
-
 $(document).ready(function () {
     // Gestionnaire de clic sur les boutons Ajouter/Supprimer (utilisation de délégués d'événements)
     $('#addRowBtn').on('click', function () {
-      if ($('.inventory-row').length < 6) {
-        addInventoryRow();
-      }
+        if ($('.inventory-row').length < 6) {
+            addInventoryRow();
+        }
     });
-  
+
     $('#removeRowBtn').on('click', function () {
-      removeInventoryRow();
+        removeInventoryRow();
     });
-  
+
     // Fonction pour ajouter une ligne d'inventaire
     function addInventoryRow() {
-      const inventory = $('#inventory');
-      const newRow = $('<div class="inventory-row"></div>');
-  
-      for (let i = 0; i < 9; i++) {
-        const itemIndex = inventory.children('.inventory-row').length * 9 + i;
-        const item = $(`<div id="inventory-item" oncontextmenu="showConfigMenu(event, 'case${itemIndex}')">Item ${itemIndex}</div>`);
-        newRow.append(item);
-      }
-  
-      inventory.append(newRow);
+        const inventory = $('#inventory');
+        const newRow = $('<div class="inventory-row"></div>');
+
+        for (let i = 0; i < 9; i++) {
+            const itemIndex = inventory.children('.inventory-row').length * 9 + i;
+            const item = $(`<div class="inventory-item" data-item-index="${itemIndex}">Item ${itemIndex}</div>`);
+            item.on('contextmenu', function (event) {
+                showConfigMenu(event, `case${$(this).data('item-index')}`);
+                return false; // Empêche le menu contextuel par défaut
+            });
+            newRow.append(item);
+        }
+
+        inventory.append(newRow);
     }
-  
+
     // Fonction pour supprimer une ligne d'inventaire
     function removeInventoryRow() {
-      const inventory = $('#inventory');
-      const rows = inventory.children('.inventory-row');
-  
-      if (rows.length > 1) {
-        rows.last().remove();
-      }
+        const inventory = $('#inventory');
+        const rows = inventory.children('.inventory-row');
+
+        if (rows.length > 1) {
+            rows.last().remove();
+        }
     }
+
     // Ajouter trois lignes d'inventaire au démarrage
     for (let i = 0; i < 3; i++) {
-      addInventoryRow();
+        addInventoryRow();
     }
-  });
-  
+});
