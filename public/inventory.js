c2b4fcb9d336772243adf94ba1c7e6037e698705
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-20 23:30:49 +0100

    Update inventory.js

--- public/inventory.js
+++ public/inventory.js
@@ -7,30 +7,30 @@ $(document).ready(function () {
         addInventoryRow();
       }
     });
-  
+
     $('#removeRowBtn').on('click', function () {
       removeInventoryRow();
     });
-  
+
     // Fonction pour ajouter une ligne d'inventaire
     function addInventoryRow() {
       const inventory = $('.inventory');
       const newRow = $('<div class="inventory-row"></div>');
-  
+
       for (let i = 0; i < 9; i++) {
         const itemIndex = inventory.children('.inventory-row').length * 9 + i;
-        const item = $(`<div class="inventory-item" oncontextmenu="showConfigMenu(event, 'case${itemIndex}')">Item ${itemIndex}</div>`);
+        const item = $(`<div class="inventory-item">Item ${itemIndex}</div>`);
         newRow.append(item);
       }
-  
+
       inventory.append(newRow);
     }
-  
+
     // Fonction pour supprimer une ligne d'inventaire
     function removeInventoryRow() {
       const inventory = $('.inventory');
       const rows = inventory.children('.inventory-row');
-  
+
       if (rows.length > 1) {
         rows.last().remove();
       }
