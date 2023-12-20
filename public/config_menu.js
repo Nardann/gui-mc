ee48cdc065b5fa70e632fa685e949cb9f356e126
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-20 23:17:36 +0100

    Update config_menu.js

--- public/config_menu.js
+++ public/config_menu.js
@@ -1,18 +1,36 @@
-function showConfigMenu(event) {
+function showConfigMenu(event, cellId) {
     event.preventDefault(); // Prevent the default context menu
+
     const configBox = document.getElementById("configBox");
+    const input1 = document.getElementById("input1");
+    const input2 = document.getElementById("input2");
+
+    // Set unique identifiers for inputs based on the cellId
+    input1.setAttribute("data-cell-id", cellId);
+    input2.setAttribute("data-cell-id", cellId);
+
+    // Set initial values based on the cellId or retrieve values from storage
+    // For example, you might retrieve stored values for each cell from a database
+
+    // Position the menu
     configBox.style.display = "block";
     configBox.style.left = event.clientX + "px";
     configBox.style.top = event.clientY + "px";
 }
 
 function saveConfiguration() {
-    const input1Value = document.getElementById("input1").value;
-    const input2Value = document.getElementById("input2").value;
+    const configBox = document.getElementById("configBox");
+    const input1 = document.getElementById("input1");
+    const input2 = document.getElementById("input2");
+
+    const cellId = input1.getAttribute("data-cell-id");
+
+    // Retrieve values from inputs and save to storage (e.g., a database) based on the cellId
+    const input1Value = input1.value;
+    const input2Value = input2.value;
 
     // Perform actions with the input values, e.g., save to a database
 
     // Hide the configuration menu after saving
-    document.getElementById("configBox").style.display = "none";
+    configBox.style.display = "none";
 }
-
