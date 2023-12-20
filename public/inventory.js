8e71617f164c989f23859b2461f12f7fd31e3a1c
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-20 23:38:47 +0100

    Update inventory.js

--- public/inventory.js
+++ public/inventory.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
 
       for (let i = 0; i < 9; i++) {
         const itemIndex = inventory.children('.inventory-row').length * 9 + i;
-        const item = $(`<div id="inventory-item-${itemIndex}" oncontextmenu="showConfigMenu(event, 'case${itemIndex}')">Item ${itemIndex}</div>`);
+        const item = $(`<div class="inventory-items" id="inventory-item-${itemIndex}" oncontextmenu="showConfigMenu(event, 'case${itemIndex}')">Item ${itemIndex}</div>`);
         newRow.append(item);
       }
 
