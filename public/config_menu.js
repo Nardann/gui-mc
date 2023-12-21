aff6b33bd53bd9f0d04e5d0ee8d8c99070e1e650
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-21 18:25:49 +0100

    Update config_menu.js

--- public/config_menu.js
+++ public/config_menu.js
@@ -1,27 +1,15 @@
 function showConfigMenu(event, caseId) {
     event.preventDefault(); // Prevent the default context menu
-
-    const configBox = document.getElementById("inventory");
-    for (let i = 0; i < 54; i++) {
-        const case${itemIndex} = document.getElementById("inventory-item-${itemIndex}");
-        case${itemIndex}.setAttribute("data-case-id", case${itemIndex});
-      }
-
-    // Set initial values based on the cellId or retrieve values from storage
-    // For example, you might retrieve stored values for each cell from a database
-
-    // Position the menu
+    const caseId = document.getElementById(caseId);
     configBox.style.display = "block";
-    configBox.style.left = event.clientX + "px";
-    configBox.style.top = event.clientY + "px";
 }
 
 function saveConfiguration() {
-    const configBox = document.getElementById("inventory");
+    const configBox = document.getElementById("configBox");
     const input1 = document.getElementById("input1");
     const input2 = document.getElementById("input2");
 
-    const cellId = input1.getAttribute("data-case-id");
+    const cellId = input1.getAttribute("data-cell-id");
 
     // Retrieve values from inputs and save to storage (e.g., a database) based on the cellId
     const input1Value = input1.value;
