e8c415198965aad8e5f8784b25ab6cf3f671f659
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-21 19:00:14 +0100

    Update inventory.js

--- public/inventory.js
+++ public/inventory.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
                 <div class="inventory-items" id="inventory-item-${itemIndex}">Item_${itemIndex}</div>
                 <div class="item-config" id="itemConfig${itemIndex}">
                     <input type="text" name="name"/>
-                    <input class="close" id="close${itemIndex}" type="button" value="close"/>
+                    <input class="close" id="close${itemIndex}" type="button" onclick="close(`itemConfig${itemIndex}`)"value="close"/>
                 </div>
             `);
 
