3f1c1f0ecd854ce100554b412e5d7d10cec6230e
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-21 18:55:16 +0100

    Update config_menu.js

--- public/config_menu.js
+++ public/config_menu.js
@@ -3,3 +3,8 @@ function openConfig(itemConfigId) {
     const configId = $("#" + itemConfigId);
     configId.show();
 }
+// Fonction pour ouvrir la configuration
+function close(itemConfigId) {
+    const configId = $("#" + itemConfigId);
+    configId.show();
+}
