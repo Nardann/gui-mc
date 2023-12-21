ee5242ffbcff1c40a27d01f29a15e98d20cf54dd
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-21 18:30:07 +0100

    Update config_menu.js

--- public/config_menu.js
+++ public/config_menu.js
@@ -4,19 +4,3 @@ function open(caseId) {
     configBox.style.display = "block";
 }
 
-function saveConfiguration() {
-    const configBox = document.getElementById("configBox");
-    const input1 = document.getElementById("input1");
-    const input2 = document.getElementById("input2");
-
-    const cellId = input1.getAttribute("data-cell-id");
-
-    // Retrieve values from inputs and save to storage (e.g., a database) based on the cellId
-    const input1Value = input1.value;
-    const input2Value = input2.value;
-
-    // Perform actions with the input values, e.g., save to a database
-
-    // Hide the configuration menu after saving
-    configBox.style.display = "none";
-}
