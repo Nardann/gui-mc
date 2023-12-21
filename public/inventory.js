1b8451b82b6bcf4b5597bba9bf5a1e5381e37ec2
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-21 18:41:25 +0100

    Update inventory.js

--- public/inventory.js
+++ public/inventory.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
         for (let i = 0; i < 9; i++) {
             const itemIndex = inventory.children('.inventory-row').length * 9 + i;
             const item = $(`
-                <div class="inventory-items" id="inventory-item-${itemIndex}">Item ${itemIndex}</div>
+                <div class="inventory-items" id="inventory-item-${itemIndex}">Item_${itemIndex}</div>
                 <div class="item-config" id="itemConfig${itemIndex}">
                     <input type="text" name="name"/>
                 </div>
