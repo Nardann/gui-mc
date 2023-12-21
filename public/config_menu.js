44936c2dd7961984782fc75282a7196375807a95
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-21 18:28:32 +0100

    Update config_menu.js

--- public/config_menu.js
+++ public/config_menu.js
@@ -1,4 +1,4 @@
-function showConfigMenu(event, caseId) {
+function open(caseId) {
     event.preventDefault(); // Prevent the default context menu
     const caseId = document.getElementById(caseId);
     configBox.style.display = "block";
