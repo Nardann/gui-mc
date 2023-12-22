6d7348d6b8186b83e4916bbf45e6d8c56e3399b7
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-22 23:12:23 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -39,42 +39,44 @@ function removeNotification(message) {
   notif_text.innerText = message;
 }
 
-for (let i = 0; i < 27; i++) {
-fetch(
-    "./items.json"
-)
-.then((response) => response.json())
-.then((data) => {
-    const itemSelect = document.getElementById(`item${i}`);
-
-    const sortedOptions = [];
-
-    for (const key in data) {
-        if (data.hasOwnProperty(key)) {
-            sortedOptions.push({ value: data[key].name, text: data[key].displayName });
-        }
-    }
-
-    // Trier le tableau par ordre alphabétique en utilisant le nom de l'élément
-    sortedOptions.sort((a, b) => a.text.localeCompare(b.text));
-
-    // Parcourir les options triées et les ajouter au menu déroulant
-    sortedOptions.forEach((optionData) => {
-        const option = document.createElement("option");
-        option.value = optionData.value;
-        option.text = optionData.text;
-        itemSelect.appendChild(option);
-    });
+function loadItem(nbrCase) {
+  for (let i = 0; i < 27; i++) {
+    fetch(
+        "./items.json"
+    )
+    .then((response) => response.json())
+    .then((data) => {
+        const itemSelect = document.getElementById(`item${i}`);
     
-
-    // Gérer la mise à jour de l'input de l'affichage de l'élément sélectionné
-    itemSelect.addEventListener("change", function () {
-        const selectedItemTextType = itemSelect.value;
-        document.getElementsByName("item_minecraft")[0].value = selectedItemTextType;
+        const sortedOptions = [];
+    
+        for (const key in data) {
+            if (data.hasOwnProperty(key)) {
+                sortedOptions.push({ value: data[key].name, text: data[key].displayName });
+            }
+        }
+    
+        // Trier le tableau par ordre alphabétique en utilisant le nom de l'élément
+        sortedOptions.sort((a, b) => a.text.localeCompare(b.text));
+    
+        // Parcourir les options triées et les ajouter au menu déroulant
+        sortedOptions.forEach((optionData) => {
+            const option = document.createElement("option");
+            option.value = optionData.value;
+            option.text = optionData.text;
+            itemSelect.appendChild(option);
+        });
         
+    
+        // Gérer la mise à jour de l'input de l'affichage de l'élément sélectionné
+        itemSelect.addEventListener("change", function () {
+            const selectedItemTextType = itemSelect.value;
+            document.getElementsByName("item_minecraft")[0].value = selectedItemTextType;
+            
+        });
+    })
+    .catch((error) => {
+        console.error("Erreur lors de la récupération des données JSON:", error);
     });
-})
-.catch((error) => {
-    console.error("Erreur lors de la récupération des données JSON:", error);
-});
+  }
 }
