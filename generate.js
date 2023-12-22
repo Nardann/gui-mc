751adea9885e65cb2a76e6768706995322b30b83
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-22 22:33:46 +0100

    Update generate.js

--- generate.js
+++ generate.js
@@ -2,6 +2,7 @@ function generateConfig(nbrCase) {
     console.log(nbrCase);
     for (let i = 0; i < nbrCase; i++) {
         var onInput = document.getElementById(`on${i}`);
+        var names = [];
         if(onInput.checked) {
             var nameInput = document.getElementById(`name${i}`);
             name[i] = nameInput.value;
