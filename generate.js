3eda4e23bf73872a5b9750c3b5cfcdb99bc7c017
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-22 22:05:25 +0100

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,1 +1,9 @@
-
+function generateConfig(nbrCase) {
+    if (!configOpen) {
+        const configId = $("#" + itemConfigId);
+        configId.css("display", "grid");
+        configOpen = true;
+    }else {
+        showNotification("⛔", "Veuillez fermer la configuration deja ouverte")
+    }
+}
