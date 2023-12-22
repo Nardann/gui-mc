4d38057b9908dd8bdc2829ecadbc7f57561df77f
Nardann <94087736+Nardann@users.noreply.github.com>
2023-12-22 22:51:04 +0100

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,8 +1,17 @@
 var names = [];
 var displayNames = [];
 function generateConfig(nbrCase) {
+    var titleInput = document.getElementById(`title`);
+        var title = titleInput.value;
     console.log(nbrCase);
+    var configDeluxemenu = `
+        menu_title: ${title}
+        size: ${nbrCase}
+        items:
+        `
     for (let i = 0; i < nbrCase; i++) {
+        
+        
         var onInput = document.getElementById(`on${i}`);
         
         if(onInput.checked) {
@@ -11,6 +20,11 @@ function generateConfig(nbrCase) {
 
             var displayNameInput = document.getElementById(`displayName${i}`);
             displayNames[i] = displayNameInput.value;
+
+
+
+            
         }
     } return { names: names, displayNames: displayNames };
+    console.log(configDeluxemenu);
 }
